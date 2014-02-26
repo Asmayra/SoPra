6affbfef0eb4c02571e03675b2bf2fe612a1c108
Philipp <blalb>
2014-02-26 16:22:14 +0100

    Main Classcomments added

--- SoPra/src/org/control/Main.java
+++ SoPra/src/org/control/Main.java
@@ -1,7 +1,11 @@
 package org.control;
 
 import org.view.MainScreen;
-
+/**
+ * MainControll Class
+ * @author Philipp
+ *
+ */
 public class Main {
 	public static void main(String[] args){
 			Main main = new Main();
