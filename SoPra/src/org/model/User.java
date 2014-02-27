8f5a6d49118a4e1189f4ffd359f1cdc99665e102
Philipp <blalb>
2014-02-27 11:41:14 +0100

    getter&setter added

--- SoPra/src/org/model/User.java
+++ SoPra/src/org/model/User.java
@@ -1,9 +1,24 @@
 package org.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 
 
 public interface User {
-	
+	public String getUsername();
+	public void setUsername(String username);
+	public String getPassword();
+	public void setPassword(String password);
+	public String getFirstname();
+	public void setFirstname(String firstname);
+	public String getLastname();
+	public void setLastname(String lastname);
+	public Date getDob();
+	public void setDob(Date dob);
+	public String getCity();
+	public void setCity(String city);
+	public String getCountry();
+	public void setCountry(String country);
 
 }
