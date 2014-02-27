f40d5838b0fc631c86f1c862c74064120f2627b8
Philipp <blalb>
2014-02-27 12:36:36 +0100

    Save&Load

--- SoPra/src/org/control/DatabaseController.java
+++ SoPra/src/org/control/DatabaseController.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.model.StandartUser;
+import org.model.User;
 
 
 public class DatabaseController {
@@ -17,6 +19,7 @@ public class DatabaseController {
 	private static DatabaseController instance;
 	
 	private DatabaseController(){
+		System.out.println("initialize DataBase");
 		initDatabaseConnection();
 	}
 	/**
@@ -37,7 +40,7 @@ public class DatabaseController {
 		Configuration cfg = new Configuration();
 	       
 	    cfg.setProperty("hibernate.connection.url", "jdbc:postgresql://ec2-54-197-238-239.compute-1.amazonaws.com:5432/dc38385afumsn0?user=yuxvjihrqgsqoz&password=DslK6nwLkoC8PINe7iouQ1F4EU&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
-	    cfg.configure("/META-INF/hibernate.xml");
+	    cfg.configure("/Hibernate.cfg.xml");
 	    cfg.setProperty("hibernate.temp.use_jdbc_metadata_defaults","false");
 	    serviceRegistryBuilder = new StandardServiceRegistryBuilder();
 	    serviceRegistryBuilder.applySettings(cfg.getProperties());
@@ -60,17 +63,27 @@ public class DatabaseController {
 	
 	/**
 	 * Saves the Object o in the DataBase
+	 * Example:
+	 * 			DatabaseController.getInstance().save(user1);
 	 * @param o 
 	 */
 	public void save(Object o){
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.persist(o);
-		session.getTransaction().commit();
-		session.close();
+		try{
+			session = sessionFactory.openSession();
+			session.beginTransaction();
+			session.persist(o);
+			session.getTransaction().commit();
+			session.close();
+		}catch(org.hibernate.exception.ConstraintViolationException e){
+			System.out.println("Username taken!");
+		}
+	
+		
 	}
 	/**
 	 * loads an Object from the Database. 
+	 * Example: 
+	 * 			user1 = (StandartUser) DatabaseController.getInstance().load(StandartUser.class, "User1");
 	 * @param c Class of the Object
 	 * @param id Id of the Object
 	 * @return Object (needs to be casted)
