e6c38159f2e603cae9ddd9bdd4748c24fc4c2f0a
Philipp <blalb>
2014-02-27 19:59:32 +0100

    Inheritance troubles resolved

--- SoPra/src/org/model/Playlist.java
+++ SoPra/src/org/model/Playlist.java
@@ -10,12 +10,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 @Entity
 @Table(name="PLAYLIST_TABLE")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Playlist {
 
 	@Id
