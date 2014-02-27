8d23fc3cae1d7f8d510aa86164eb9035d0537d10
SebastianRoth <s.roth@wwu.de>
2014-02-27 14:31:45 +0100

    ProfilScreen in bearbeitung

--- SoPra/src/org/view/screens/Center/ProfileScreen.java
+++ SoPra/src/org/view/screens/Center/ProfileScreen.java
@@ -1,7 +1,46 @@
 package org.view.screens.Center;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
-public class ProfileScreen extends JPanel{
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+import org.model.User;
+
+public class ProfileScreen extends JPanel {
+	private User userProfile;
+
+	public ProfileScreen(User selectedUser) {
+
+		JPanel main = new JPanel();
+		main.setLayout(new BorderLayout(0, 0));
+
+		JPanel userOverview = new JPanel();
+		main.add(userOverview, BorderLayout.NORTH);
+		userOverview.setLayout(new BorderLayout(0, 0));
+
+		JPanel userPicture = new JPanel();
+
+		userOverview.add(userPicture, BorderLayout.WEST);
+
+		JPanel userData = new JPanel();
+		userOverview.add(userData, BorderLayout.CENTER);
+
+		JPanel buttons = new JPanel();
+		userOverview.add(buttons, BorderLayout.SOUTH);
+
+		JPanel userName = new JPanel();
+		userOverview.add(userName, BorderLayout.NORTH);
+
+		JScrollPane userContentScroll = new JScrollPane();
+		main.add(userContentScroll, BorderLayout.WEST);
+		userContentScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		JPanel userContent = new JPanel();
+		main.add(userContent, BorderLayout.SOUTH);
+		userContentScroll = new JScrollPane(userContent);
+
+	}
 
 }
