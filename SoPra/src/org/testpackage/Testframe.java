c677e1864690eda0e388eb1fb9072b7a4ea75b78
m_pfen02 <Michael@10.66.32.114>
2014-02-27 16:06:06 +0100

    fallback

--- SoPra/src/org/testpackage/Testframe.java
+++ SoPra/src/org/testpackage/Testframe.java
@@ -7,9 +7,6 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 
-import org.control.RegControl;
-import org.view.RegScreen;
-
 public class Testframe extends JFrame {
 
 	/**
@@ -25,26 +22,48 @@ public class Testframe extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					RegControl control = RegControl.getInstance();
-					control.showRegistration();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Testframe() {
-		
-		
-	}
-	public static String CurrentDir(){
-		String path=System.getProperty("java.class.path");
-		String FileSeparator=(String)System.getProperty("file.separator");
-		return path.substring(0, path.lastIndexOf(FileSeparator)+1);
-	}
-	
-}
+					Testframe frame = new Testframe();
+					frame.setVisible(true);
+ 			} catch (Exception e) {
+ 					e.printStackTrace();
+ 				}
+ 			}
+ 		});
+ 	}
+ 
+ 	/**
+ 	 * Create the frame.
+ 	 */
+ 	public Testframe() {
+ 		String curDir = System.getProperty("user.dir");
+ 		String delimeter_symbol = System.getProperty("file.separator");
+ 		String new_dir = curDir +delimeter_symbol+"Names.abf";
+ 		String path= System.getProperty("java.class.path");
+ 		String Data_Location = CurrentDir()+"Names.abf";
+ 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 		setBounds(100, 100, 450, 300);
+ 		
+ 		contentPane = new JPanel();
+ 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+ 		setContentPane(contentPane);
+    	contentPane.setLayout(null);
+ 		
+ 		JScrollPane scrollPane = new JScrollPane();
+ 		scrollPane.setBounds(40, 40, 400, 100);
+ 		contentPane.add(scrollPane);
+ 		
+ 		JTextArea TextArea_output = new JTextArea();
+ 		scrollPane.setViewportView(TextArea_output);
+ 		TextArea_output.setText("Text Area with ways" +"\n");
+ 		TextArea_output.append(path+"\n");
+ 		TextArea_output.append(new_dir +"\n");
+ 		TextArea_output.append(Data_Location);
+ 		
+ 	}
+ 	public static String CurrentDir(){
+ 		String path=System.getProperty("java.class.path");
+ 		String FileSeparator=(String)System.getProperty("file.separator");
+ 		return path.substring(0, path.lastIndexOf(FileSeparator)+1);
+ 	}
+ 	
+ }
