7b6ca8660af1e4e131e657223dc82b9cbfbca0a4
Philipp <blalb>
2014-02-27 10:13:38 +0100

    Ids added

--- SoPra/src/org/model/Genre.java
+++ SoPra/src/org/model/Genre.java
@@ -2,10 +2,16 @@ package org.model;
 
 import java.util.List;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 
 public class Genre {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private int genreId;
 	private String name;
 	private List<Genre> subGenres;
 
--- SoPra/src/org/model/Label.java
+++ SoPra/src/org/model/Label.java
@@ -1,6 +1,13 @@
 package org.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Label {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private int LabelId;
 
 	public Label() {
 		// TODO Auto-generated constructor stub
