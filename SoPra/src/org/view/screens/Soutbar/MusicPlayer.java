b9825e94ce17ddefca7ec9d69a8c9610273ec4be
unim95 <unim95@UNIm95-Thinkpad>
2014-02-27 17:03:22 +0100

    Begin of player implementation.

--- SoPra/src/org/view/screens/Soutbar/MusicPlayer.java
+++ SoPra/src/org/view/screens/Soutbar/MusicPlayer.java
@@ -1,7 +1,21 @@
 package org.view.screens.Soutbar;
 
 import javax.swing.JPanel;
+import javax.swing.*;
 
 public class MusicPlayer extends JPanel {
-
+	private JButton Btn_Stop;
+	private JButton Btn_Start;
+	private JButton Btn_Forward;
+	private JButton Btn_Back;
+	JProgressBar ProgressBar = new JProgressBar();
+/*=============================================================================*/
+	public void main(){
+		initialize();
+		System.out.println("qwe");
+	}
+	public void initialize(){
+		JPanel Panel = new JPanel();
+		//Panel.isVisible();
+	}
 }
