6d278519112d6a522db8eaa0e830a2b51f23b9d1
Philipp <blalb>
2014-02-28 12:01:58 +0100

    ...

--- SoPra/src/main/java/org/view/screens/Center/HomeScreen.java
+++ SoPra/src/main/java/org/view/screens/Center/HomeScreen.java
@@ -3,11 +3,15 @@ package org.view.screens.Center;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.model.Post;
+import org.model.User;
 import org.view.MainScreen;
 
 public class HomeScreen extends JPanel{
@@ -16,6 +20,21 @@ public class HomeScreen extends JPanel{
 		JLabel title = new JLabel("HOME");
 		title.setPreferredSize(new Dimension(400,20));
 		this.add(title,BorderLayout.NORTH);
+		//***********************************************
+			User tst = new User();
+			Post pst = new Post();
+			
+			Collection<Post> psts = new LinkedList<Post>();
+			tst.addPosts(pst);
+			tst.setUsername("FirstTestUser");
+			
+			this.add(pst.create(),BorderLayout.CENTER);
+			
+		
+		
+		
+		
+		//***********************************************
 		
 	}
 
