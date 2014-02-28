d336e3f4e01a57220cd29da3f3556069c848248a
m_pfen02 <michael.Pfennings@wwu.de>
2014-02-28 13:57:48 +0100

    testklassen

---
+++ SoPra/src/test/java/org/control/LoginControlTest.java
@@ -0,0 +1,46 @@
+package org.control;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.model.User;
+
+public class LoginControlTest {
+	
+	
+	@BeforeClass
+	public static void createTestUser(){
+		User newUser = new User();
+		
+		newUser.setUsername("LoginTestUser");
+		newUser.setSalt(PasswordControl.generateSalt());
+		newUser.setPassword(PasswordControl.encodePassword("Kennwort", newUser.getSalt()));
+		
+		
+		try{
+			DatabaseController.getInstance().save(newUser);
+		}catch(IOException e){
+			System.out.println("Username already taken!");
+		}
+	}
+
+	@Test
+	public void loginCheckTest() {
+		assertTrue(LoginControl.getInstance().checkLogin("LoginTestUser", "Kennwort"));
+		assertFalse(LoginControl.getInstance().checkLogin("LoginTestUser", "kennwort"));
+		assertFalse(LoginControl.getInstance().checkLogin("LoginTestUser", ""));
+	}
+	
+	
+	
+	
+	@AfterClass
+	public static void deleteTestUser(){
+		//...
+	}
+
+}
