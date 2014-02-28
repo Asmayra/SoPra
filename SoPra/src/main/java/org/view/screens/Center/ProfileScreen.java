5ba80d9a88ed643c5901a97567c3ed02a106bd02
SebastianRoth <s.roth@wwu.de>
2014-02-28 12:20:15 +0100

    Profile Screen -> in Bearbeitung

--- SoPra/src/main/java/org/view/screens/Center/ProfileScreen.java
+++ SoPra/src/main/java/org/view/screens/Center/ProfileScreen.java
@@ -6,8 +6,10 @@
 package org.view.screens.Center;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -34,6 +36,11 @@ public class ProfileScreen extends JPanel {
 	private JButton ignore;
 	private JButton media;
 	private JButton follow;
+	private JLabel lblVornameame;
+	private JLabel lblNachname;
+	private JLabel lblStadt;
+	private JLabel lblLand;
+	private JLabel lblAlter;
 	
 	
 	public ProfileScreen(User selectedUser) {
@@ -58,6 +65,45 @@ public class ProfileScreen extends JPanel {
 
 		userData = new JPanel();
 		userData.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		lblVornameame = new JLabel("Vorname:");
+		GridBagConstraints gbc_lblVornameame = new GridBagConstraints();
+		gbc_lblVornameame.fill = GridBagConstraints.BOTH;
+		gbc_lblVornameame.insets = new Insets(0, 0, 0, 5);
+		gbc_lblVornameame.gridx = 0;
+		gbc_lblVornameame.gridy = 0;
+		userData.add(lblVornameame, gbc_lblVornameame);
+		
+		lblNachname = new JLabel("Nachname:");
+		GridBagConstraints gbc_lblNachname = new GridBagConstraints();
+		gbc_lblNachname.fill = GridBagConstraints.BOTH;
+		gbc_lblNachname.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNachname.gridx = 1;
+		gbc_lblNachname.gridy = 1;
+		userData.add(lblNachname, gbc_lblNachname);
+		
+		lblStadt = new JLabel("Stadt:");
+		GridBagConstraints gbc_lblStadt = new GridBagConstraints();
+		gbc_lblStadt.fill = GridBagConstraints.BOTH;
+		gbc_lblStadt.insets = new Insets(0, 0, 0, 5);
+		gbc_lblStadt.gridx = 2;
+		gbc_lblStadt.gridy = 2;
+		userData.add(lblStadt, gbc_lblStadt);
+		
+		lblLand = new JLabel("Land:");
+		GridBagConstraints gbc_lblLand = new GridBagConstraints();
+		gbc_lblLand.fill = GridBagConstraints.BOTH;
+		gbc_lblLand.insets = new Insets(0, 0, 0, 5);
+		gbc_lblLand.gridx = 3;
+		gbc_lblLand.gridy = 3;
+		userData.add(lblLand, gbc_lblLand);
+		
+		lblAlter = new JLabel("Alter: ");
+		GridBagConstraints gbc_lblAlter = new GridBagConstraints();
+		gbc_lblAlter.fill = GridBagConstraints.BOTH;
+		gbc_lblAlter.gridx = 4;
+		gbc_lblAlter.gridy = 4;
+		userData.add(lblAlter, gbc_lblAlter);
 		
 		userOverview.add(userData, BorderLayout.CENTER);
 
