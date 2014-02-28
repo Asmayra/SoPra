86370ade35e851657e523a59bacb213169f63456
m_pfen02 <Michael@10.66.32.114>
2014-02-28 12:49:13 +0100

    + Login Funktionen + Registrierung fertig + Javadoc

--- SoPra/src/main/java/org/control/LoginControl.java
+++ SoPra/src/main/java/org/control/LoginControl.java
@@ -1,13 +1,29 @@
 package org.control;
+
+import org.model.User;
+
 /**
- * Steuert den Login eines Nutzers
+ * Steuert den Login eines Nutzers. Ist ein Singleton
  * @author Michael Pfennings, Mattias Schoenke
  *
  */
 
 public class LoginControl {
 	
+	private static LoginControl instance = null;
+	private LoginControl(){
+		
+	}
 	
+	
+	public static LoginControl getInstance(){
+		if(instance == null)
+		{
+			instance = new LoginControl();
+		}
+		
+		return instance;
+	}
 	
 	/**
 	 * Überprüft ob das Passwort zum mit dem des angegebenen Benutzers übereinstimmt
@@ -15,19 +31,27 @@ public class LoginControl {
 	 * @param passwort Passwort das beim einloggen angegeben wurde
 	 * @return
 	 */
-	public boolean checkLogin(String nutzer, String passwort)
+	public boolean checkLogin(String username, String password)
 	{
-		//getUser(nutzer)
+		if( ( username == null || username.equals("") ) || ( password == null || password.equals("") ) )
+			return false;
+		User loginUser = (User) DatabaseController.getInstance().load(User.class, username);
 		
-		String salt = "";
+		if (loginUser != null ){
+			String salt = loginUser.getSalt();
 		
-		//getSalt()
+			
+			String pass = PasswordControl.encodePassword(password, salt);
+			
+			String userPass = loginUser.getPassword();
+			
+			return PasswordControl.comparePasswords(pass, userPass);		
+			
+		}
+		else{
+			return false;
+		}
 		
-		int pass = PasswordControl.encodePassword(passwort, salt);
-		
-		int userPass = 0;
-		
-		return PasswordControl.comparePasswords(pass, userPass);		
 	}
 
 }
--- SoPra/src/main/java/org/control/PasswordControl.java
+++ SoPra/src/main/java/org/control/PasswordControl.java
@@ -2,9 +2,20 @@ package org.control;
 
 import java.util.Random;
 
+/**
+ * Stellt statische Methoden zur Verwaltung von Passwörtern bereit
+ * @author Michael Pfennings, Mattias Schoenke
+ *
+ */
+
 public class PasswordControl {
 	
-	
+	/**
+	 * Verschlüsselt das Passwort
+	 * @param pass unverschlüsseltes Passwort
+	 * @param salt Salt zur Verschlüsselung
+	 * @return verschlüsseltes Passwort
+	 */
 	public static String encodePassword(String pass, String salt)
 	{
 		String password = pass + salt;
@@ -12,11 +23,21 @@ public class PasswordControl {
 		return "" + password.hashCode();
 	}
 	
-	public static boolean comparePasswords(int pass1, int pass2)
+	/**
+	 * Vergleicht zwei Passwörter
+	 * @param pass1 Passwort
+	 * @param pass2 Passwort
+	 * @return True wenn gleich und keines der beiden null, sonst false
+	 */
+	public static boolean comparePasswords(String pass1, String pass2)
 	{
-		return pass1 == pass2;
+		return pass1 != null && pass2 != null && pass1.equals(pass2);
 	}
 	
+	/**
+	 * Generiert einen zufälligen Salt aus folgenden Zeichen: "abcdefghijklmnopqrstuvwxyz1234567890!§$%&/()=?"
+	 * @return zufälligen Salt
+	 */
 	public static String generateSalt()
 	{
 		String chars = "abcdefghijklmnopqrstuvwxyz1234567890!§$%&/()=?";
