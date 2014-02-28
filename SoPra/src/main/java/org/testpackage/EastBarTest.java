e871979d5ca1808ef81bdce7cf745d7c224a3e5c
t_lang20 <t_lang20@wwu.de>
2014-02-28 14:32:05 +0100

    eastbar init

---
+++ SoPra/src/main/java/org/testpackage/EastBarTest.java
@@ -0,0 +1,47 @@
+package org.testpackage;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.view.screens.EastBar.DiscoverElement;
+import org.view.screens.EastBar.DiscoverMiniScreen;
+import org.view.screens.EastBar.EastBar;
+
+public class EastBarTest {
+
+    private void display() {
+        
+    	JFrame f = new JFrame("EastBar");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        EastBar eb = new EastBar();
+        
+        DiscoverMiniScreen miniScreen = DiscoverMiniScreen.getInstance();
+        DiscoverElement discover = new DiscoverElement();
+        miniScreen.setDiscoverOne(discover);
+        
+        f.add(eb);
+        f.setPreferredSize(new Dimension(400,600));
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+    	
+    }
+    
+	public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+
+            //@Override
+            public void run() {
+                new EastBarTest().display();
+            }
+        });
+
+	}
+
+}
