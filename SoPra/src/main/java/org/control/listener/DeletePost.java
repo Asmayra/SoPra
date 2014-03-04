6657afbab48ab9be98877c735573fd341e06e784
Philipp <blalb>
2014-03-04 16:29:50 +0100

    delete Post added

---
+++ SoPra/src/main/java/org/control/listener/DeletePost.java
@@ -0,0 +1,37 @@
+package org.control.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+
+import org.control.DatabaseController;
+import org.model.Post;
+import org.model.User;
+
+public class DeletePost implements ActionListener {
+	//the selected Post
+	private Post selection; 
+	/**
+	 * 
+	 * @param selection Post to delete
+	 */
+	public DeletePost(Post selection){
+		this.selection = selection;
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		Iterator<Post> it = selection.getAutor().getPosts().iterator();
+		while(it.hasNext()){
+			Post temp = it.next();
+			if(temp.equals(selection)){
+				User usr = temp.getAutor();
+				usr.getPosts().remove(selection);
+				temp.setAutor(null);
+				DatabaseController.getInstance().update(usr);
+			}
+		}
+	}
+	
+
+
+}
