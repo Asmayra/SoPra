af25e1fdeb4f3a967462e5e6d40c3a7306411ab6
Philipp <blalb>
2014-03-04 14:02:39 +0100

    max muster entfernt

--- SoPra/src/main/java/org/control/Main.java
+++ SoPra/src/main/java/org/control/Main.java
@@ -18,25 +18,7 @@ public class Main {
 			//DiscoverControl discContr = new DiscoverControl();
 			
 			//****************************
-				//TestUser
-				//Passwort: passwort
-				User testUser = (User) DatabaseController.getInstance().load(User.class,"MaxMuster");
-				Post pst1 = new Post();
-				pst1.setMessage("Ich bin jetzt auch bei soPra");
-				Post pst2 = new Post();
-				pst2.setMessage("Hier kann man nix machen");
-				testUser.addPosts(pst1);
-				testUser.addPosts(pst2);
-				
-				try {
-					DatabaseController.getInstance().save(pst1);
-					DatabaseController.getInstance().save(pst2);
-					DatabaseController.getInstance().save(testUser);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
+		
 			//****************************		
 	}
 }
