72c67cd6f22a7d1225fc58286e708532f2eaca66
Philipp <blalb>
2014-03-04 16:51:01 +0100

    Posts linked with profilescreen

---
+++ SoPra/src/main/java/org/control/listener/OpenProfile.java
@@ -0,0 +1,42 @@
+package org.control.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import org.model.User;
+import org.view.MainScreen;
+import org.view.screens.Center.ProfileScreen;
+
+public class OpenProfile implements MouseListener{
+	User usr;
+	
+	
+	public OpenProfile(User usr){
+		this.usr =usr;
+	}
+	
+	public void mouseClicked(MouseEvent arg0) {
+		MainScreen.getInstance().updateCenter(new ProfileScreen(usr));
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
