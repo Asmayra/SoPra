f48cb3a01066e1a093bd95500f0d4ccf24f5493f
Michael <michael.Pfennings@wwu.de>
2014-03-05 14:20:03 +0100

    Listener in richtiges Paket verschoben

---
+++ SoPra/src/main/java/org/control/listener/IgnoreButtonListener.java
@@ -0,0 +1,29 @@
+package org.control.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import org.control.DatabaseControl;
+import org.control.LoginControl;
+import org.view.screens.Center.ProfileScreen;
+
+public class IgnoreButtonListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent arg0) {
+		if (((ProfileScreen) ((JButton) arg0.getSource()).getParent()).getIgnore()) {
+			LoginControl.getInstance().getCurrentUser().unignore(((ProfileScreen) ((JButton) arg0.getSource()).getParent()).getUserProfile());
+			((ProfileScreen) ((JButton) arg0.getSource()).getParent()).setIgnore(false);
+			((JButton) arg0.getSource()).setText("ignore");
+			DatabaseControl.getInstance().update(LoginControl.getInstance().getCurrentUser());
+		} else {
+			LoginControl.getInstance().getCurrentUser().ignore(((ProfileScreen) ((JButton) arg0.getSource()).getParent()).getUserProfile());
+			((ProfileScreen) ((JButton) arg0.getSource()).getParent()).setIgnore(true);
+			((JButton) arg0.getSource()).setText("unignore");
+			DatabaseControl.getInstance().update(LoginControl.getInstance().getCurrentUser());
+		}
+		
+	}
+
+}
