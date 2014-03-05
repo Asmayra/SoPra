d5977609539ff112490e16e8bcaa5acbc38b5c62
t_mich04 <t_mich04@webstuhl.uni-muenster.de>
2014-03-05 14:35:48 +0100

    Listenerstuff geändert

--- SoPra/src/main/java/org/control/listener/MoreDiscoverButtonListener.java
+++ SoPra/src/main/java/org/control/listener/MoreDiscoverButtonListener.java
@@ -5,30 +5,11 @@ import java.awt.event.ActionListener;
 import org.view.MainScreen;
 import org.view.screens.Center.DiscoverExtendedScreen;
 
-<<<<<<< HEAD
-=======
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
-import org.control.LoadImageControl;
-import org.view.screens.EastBar.DiscoverElement;
-import org.view.screens.EastBar.DiscoverMiniScreen;
-import org.view.screens.EastBar.EastBar;
->>>>>>> branch 'master' of https://github.com/Asmayra/SoPra.git
-
 public class MoreDiscoverButtonListener implements ActionListener{
 
 	public void actionPerformed(ActionEvent arg0) {
-<<<<<<< HEAD
 		MainScreen.getInstance().showDiscoverExtendedScreen(DiscoverExtendedScreen.getInstance());
-=======
-		System.out.println("mehr Empfehlungen");
-		
-		JLabel pictureLabel = new JLabel(LoadImageControl.loadImageIcon("Placeholder.jpg"));
-		//DiscoverMiniScreen.getInstance().setDiscoverOne(new DiscoverElement(UUID.randomUUID().toString(), pictureLabel, UUID.randomUUID().toString()));
 
->>>>>>> branch 'master' of https://github.com/Asmayra/SoPra.git
 	}
 
 }
