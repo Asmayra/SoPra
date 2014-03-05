22362fbb7a5860fb7535e70e9cb7369090df98e6
SebastianRoth <s.roth@wwu.de>
2014-03-05 12:50:39 +0100

    Profile Panels

--- SoPra/src/main/java/org/view/ProfilePlaylistPanel.java
+++ SoPra/src/main/java/org/view/ProfilePlaylistPanel.java
@@ -1,14 +1,54 @@
 package org.view;
 
+import java.util.List;
+
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import org.model.Playlist;
+import org.model.Song;
+import org.model.User;
+
 public class ProfilePlaylistPanel extends JPanel {
-	
-	private String[] columns = new String[] {"Interpret","Title","Album","Länge","Community Rating","Favor"};
-	private Object[][] songs;
+
+	private String[] columns = new String[] { "Name", "Ersteller", "#Lieder", "Länge", "Favor" };
+	private Object[][] context;
 	private DefaultTableModel model;
 	private JTable table;
 
+	public ProfilePlaylistPanel(User u) {
+
+		model = new DefaultTableModel(context, columns) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return column == 4;
+			}
+		};
+		table.setFillsViewportHeight(true);
+		table = new JTable();
+		table.setModel(model);
+		List<Playlist> songs = u.getPlaylists();
+		for (int i = 0; i < songs.size(); i++) {
+			String Name = songs;
+			String title = songs.get(i).getTitle();
+			boolean favored;
+			if (u.getFavorites().contains(songs.get(i))) {
+				favored = true;
+			} else {
+				favored = false;
+			}
+			String album;
+			if (songs.get(i).getAlbum() == null) {
+				album = " - ";
+			} else {
+				album = songs.get(i).getAlbum().getName();
+			}
+			String playtime = ((Integer) songs.get(i).getPlaytime()).toString();
+			String comRating = ((Integer) songs.get(i).getVrgRating()).toString();
+			Object[] entry = { interpret, title, album, playtime, comRating, favored };
+			model.addRow(entry);
+		}
+
+	}
 }
--- SoPra/src/main/java/org/view/ProfileSongsPanel.java
+++ SoPra/src/main/java/org/view/ProfileSongsPanel.java
@@ -6,52 +6,51 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import org.control.LoginControl;
 import org.model.Album;
 import org.model.Song;
 import org.model.User;
 
-
 public class ProfileSongsPanel extends JPanel {
-	
-	private String[] columns = new String[] {"Interpret","Title","Album","Länge","Community Rating","Favor"};
-	private Object[][] songs;
+
+	private String[] columns = new String[] { "Interpret", "Title", "Album", "Länge", "Community Rating", "Favor" };
+	private Object[][] context;
 	private DefaultTableModel model;
 	private JTable table;
-	
-	public ProfileSongsPanel(User u){
-		
-		model = new DefaultTableModel(songs, columns){
-			 @Override
-			   public boolean isCellEditable(int row, int column) {
-			       return column == 5;
-			   }
+
+	public ProfileSongsPanel(User u) {
+
+		model = new DefaultTableModel(context, columns) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return column == 5;
+			}
 		};
 		table.setFillsViewportHeight(true);
 		table = new JTable();
 		table.setModel(model);
 		List<Song> songs = u.getOwnSongs();
-		for (int i = 0; i< u.getOwnSongs().size();i++){
-			String interpret = u.getUsername();
+		for (int i = 0; i < songs.size(); i++) {
+			String interpret = songs.get(i).getInterpret();
 			String title = songs.get(i).getTitle();
 			boolean favored;
-			if(u.getFavorites().contains(songs.get(i))){
+			if (LoginControl.getInstance().getCurrentUser().getFavorites().contains(songs.get(i))) {
 				favored = true;
 			} else {
 				favored = false;
 			}
 			String album;
-			if (songs.get(i).getAlbum()==null){
+			if (songs.get(i).getAlbum() == null) {
 				album = " - ";
 			} else {
 				album = songs.get(i).getAlbum().getName();
 			}
 			String playtime = ((Integer) songs.get(i).getPlaytime()).toString();
-			String comRating =((Integer) songs.get(i).getVrgRating()).toString();
-			Object[] entry = {interpret,title,album,playtime,comRating,favored};
+			String comRating = ((Integer) songs.get(i).getVrgRating()).toString();
+			Object[] entry = { interpret, title, album, playtime, comRating, favored };
 			model.addRow(entry);
 		}
-		
-		
+
 	}
 
 }
