06bb119e074925869d44667e411321bdbbbd4e77
unim95 <unim95@UNIm95-Thinkpad>
2014-03-06 12:08:27 +0100

    Adding some features

--- SoPra/src/main/java/org/view/screens/Southbar/MusicPlayer.java
+++ SoPra/src/main/java/org/view/screens/Southbar/MusicPlayer.java
@@ -2,8 +2,9 @@ package org.view.screens.Southbar;
 
 import javax.swing.*;
 import java.awt.*;
-
 import javax.swing.event.*;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 /**
  * A Structure Class for the MusicPlayer
@@ -11,16 +12,18 @@ import javax.swing.event.*;
  *
  */
 public class MusicPlayer extends JPanel {
-	private JButton Btn_Stop = new JButton("Stop");
-	private JButton Btn_Play= new JButton("Play/Pause");
-	private JButton Btn_Forward= new JButton("Fwd");
-	private JButton Btn_Back= new JButton("Bck");
+	private JButton Btn_Stop = new JButton("Stop ");
+	private JButton Btn_Play= new JButton("Play/Pause ");
+	private JButton Btn_Forward= new JButton("Fwd ");
+	private JButton Btn_Back= new JButton("Bck ");
 	private JSlider volume_slider = new JSlider();
 	private GridBagConstraints gbc = new GridBagConstraints();
-	private JLabel Volume_Label =new JLabel("Volume %");
+	private JLabel Volume_Label =new JLabel("50%");
 	private JLabel Label =new JLabel("Placeholder 4 seek bar");
 	
+	private double Music_gain = 0.5;
 	public MusicPlayer(){
+		
 		this.setLayout(new GridBagLayout());
 		gbc.gridx=0;
 		gbc.gridy=0;
@@ -45,14 +48,30 @@ public class MusicPlayer extends JPanel {
 		volume_slider.setPaintLabels(true);
 		volume_slider.setPaintTicks(true);
 		volume_slider.setMajorTickSpacing(20);
+		volume_slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				// TODO Auto-generated method stub
+				Music_gain = volume_slider.getValue();
+				Volume_Label.setText(String.valueOf(Music_gain).substring(0, String.valueOf(Music_gain).length()-2) +"%");
+				Music_gain=Music_gain/100;
+				/*try {
+					player.setGain(Music_gain);
+				} catch (BasicPlayerException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}*/
+			}
+			
+		});
 		gbc.gridx=5;
 		gbc.gridy=0;
-		add(Btn_Forward,gbc);
+		add(volume_slider,gbc);
 		
 		gbc.gridx=6;
 		gbc.gridy=0;
 		add(Volume_Label,gbc);
-		
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		this.setMinimumSize(new Dimension(860,600));
 	}
 /*=============================================================================*/
 	/*
