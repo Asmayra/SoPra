ffd5ed5e13d448c80b105bf276927d24959166b3
mats391 <m.schoenke@gmail.com>
2014-03-06 10:08:45 +0100

    Message irgendwas RegControlTest MailboxControlTest

--- SoPra/src/main/java/org/model/Message.java
+++ SoPra/src/main/java/org/model/Message.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
