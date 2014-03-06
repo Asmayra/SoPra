86e1cac396f6c23d10035d177f41456e3c71a0af
t_lang20 <t_lang20@wwu.de>
2014-03-06 10:34:44 +0100

    edited load/saveimage

--- SoPra/src/main/java/org/control/LoadImageControl.java
+++ SoPra/src/main/java/org/control/LoadImageControl.java
@@ -37,8 +37,13 @@ public class LoadImageControl {
 	public static BufferedImage loadBufferedImage(String imagePath){
 		BufferedImage image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_RGB);
 		try {
-			image = ImageIO.read(ClassLoader.getSystemClassLoader().getResource(imagePath));
-			//image = ImageIO.read(new File(System.getProperty("user.dir")+"\\src\\main\\resources\\"+imagePath));
+			if(imagePath == null || imagePath == ""){
+				image = ImageIO.read(ClassLoader.getSystemClassLoader().getResource("placeholder.jpg"));
+			} else{
+				String filepath = System.getProperty("user.dir")+"\\SoPraMediaDatabase\\"+LoginControl.getInstance().getCurrentUser().getUsername()+"\\";
+				image = ImageIO.read(new File(filepath));
+			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- SoPra/src/main/java/org/control/SaveImageControl.java
+++ SoPra/src/main/java/org/control/SaveImageControl.java
@@ -1,7 +1,16 @@
 package org.control;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+/**
+ * 
+ * @author Tim Lange
+ *
+ */
 public class SaveImageControl {
 
 	/**
@@ -10,16 +19,31 @@ public class SaveImageControl {
 	 * @param username for which user the cover is to be saved
 	 */
 	public static void saveBufferedImage(BufferedImage image, String username){
-		
+		saveBufferedImage(image, username, "avatar.jpg");
 	}
 	
 	/**
-	 * save image in userdictory with specific filename(with extension)
+	 * save image with specific filename(with extension)
 	 * @param image Buffered image
 	 * @param username for which user the image is to be saved
 	 * @param filename specific filename(with extension eg. "filename.jpg") 
 	 */
 	public static void saveBufferedImage(BufferedImage image, String username, String filename){
 		
+		String extension, filenameWithoutExtension, filepath;
+		filenameWithoutExtension = filename.substring(0, filename.indexOf("."));
+		extension = filename.substring(filename.indexOf(".")+1, filename.length());
+		
+		filepath = System.getProperty("user.dir")+"\\SoPraMediaDatabase\\"+username+"\\";
+
+		//create folder if needed
+		new File(filepath+filename).mkdirs();
+
+		try {
+            ImageIO.write(image, extension, new File(filepath + filenameWithoutExtension + "."+extension));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
 	}
 }
