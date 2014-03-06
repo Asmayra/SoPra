a4a94fe8635e62aa41cf9df893dc5d14197816ae
t_lang20 <t_lang20@wwu.de>
2014-03-06 09:41:09 +0100

    initial saveimagecontrol

---
+++ SoPra/src/main/java/org/control/SaveImageControl.java
@@ -0,0 +1,14 @@
+package org.control;
+
+import java.awt.image.BufferedImage;
+
+public class SaveImageControl {
+
+	public static void saveBufferedImage(BufferedImage image){
+		
+	}
+	
+	public static void saveBufferedImage(BufferedImage image, String username){
+		
+	}
+}
