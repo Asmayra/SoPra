9410c30643240b535334f9a157d2a38e28d00949
mats391 <m.schoenke@gmail.com>
2014-03-06 16:44:27 +0100

    AdminHomeScreen wird auch immer angezeigt

--- SoPra/src/main/java/org/control/listener/HomeScreenButtonListener.java
+++ SoPra/src/main/java/org/control/listener/HomeScreenButtonListener.java
@@ -3,14 +3,19 @@ package org.control.listener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import org.control.LoginControl;
 import org.view.MainScreen;
+import org.view.screens.Center.AdminHomeScreen;
 import org.view.screens.Center.HomeScreen;
 
 public class HomeScreenButtonListener implements ActionListener  {
 
 	
 	public void actionPerformed(ActionEvent e) {
-		MainScreen.getInstance().showHomeScreen(HomeScreen.getInstance());
+		if(LoginControl.getInstance().getCurrentUser().getRights().equals("Admin"))
+			MainScreen.getInstance().updateCenter(new AdminHomeScreen());
+		else
+			MainScreen.getInstance().updateCenter(HomeScreen.getInstance());
 		
 	}
 
