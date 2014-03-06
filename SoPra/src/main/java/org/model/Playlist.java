113896979c1edb410e2cb92bbe0b8fa9f0cec9a3
SebastianRoth <s.roth@wwu.de>
2014-03-06 13:50:18 +0100

    kleine änderungen bezüglich der Favorites

--- SoPra/src/main/java/org/model/Playlist.java
+++ SoPra/src/main/java/org/model/Playlist.java
@@ -1,6 +1,5 @@
 package org.model;
 
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -20,71 +19,83 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.swing.JOptionPane;
+
 @Entity
-@Table(name="PLAYLIST_TABLE")
+@Table(name = "PLAYLIST_TABLE")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Playlist {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)	
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int playlistId;
 	@ManyToOne
 	private User owner;
 	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private Set<Song> songs;
 	private String name;
-	
-	public Playlist(){
-		
+
+	public Playlist() {
+
 	}
-	
-	public Playlist (User u){
+
+	public Playlist(User u) {
 		owner = u;
-		
+
 	}
-	
-	public User getOwner(){
+
+	public User getOwner() {
 		return this.owner;
 	}
-	
-	public boolean contains(Song s){
-		if (songs.contains(s)){
+
+	public boolean contains(Song s) {
+		if (songs.contains(s)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public Collection<Song> getSongs() {
 		Collection<Song> coll = new ArrayList<Song>(this.songs);
 		return coll;
 	}
+
 	public void setSongs(List<Song> songs) {
 		this.songs = new TreeSet<Song>(songs);
-	}	
+	}
+
 	public int getPlaylistId() {
 		return playlistId;
 	}
+
 	public void setPlaylistId(int playlistId) {
 		this.playlistId = playlistId;
 	}
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) throws IllegalArgumentException {
-		if (name == "Favorites"){
-			for (int i = 0; i <this.getOwner().getPlaylists().size();i++){
-				if (this.getOwner().getPlaylists().get(i).getName()=="Favorites"){
-					throw new IllegalArgumentException( "Nur eine Playlist mit Namen Favorites erlaubt." ); 
-				}
+
+	private void testName(String name) throws IllegalArgumentException {
+		for (int i = 0; i < owner.getPlaylists().size(); i++) {
+			if (name.equals(owner.getPlaylists().get(i).getName())) {
+				throw new IllegalArgumentException();
 			}
 		}
-		this.name = name;
 	}
 
+	public void setFavorite() {
+		this.setName("Favorites");
+	}
 
-	
-	
-	
+	public void setName(String name) {
+		try {
+			testName(name);
+			this.name = name;
+		} catch (IllegalArgumentException e) {
+			JOptionPane.showMessageDialog(null, "Jede ihrer Playlists braucht einen eigentständigen Namen.\n Bitte wählen sie einen neuen, unbenutzten Namen.");
+		}
 
+	}
 
 }
