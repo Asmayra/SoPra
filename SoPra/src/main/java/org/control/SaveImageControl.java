1a676de397fe649ff4fe69a396557c4c88acce9f
t_lang20 <t_lang20@wwu.de>
2014-03-06 09:48:19 +0100

    edited saveimagecontrol

--- SoPra/src/main/java/org/control/SaveImageControl.java
+++ SoPra/src/main/java/org/control/SaveImageControl.java
@@ -4,11 +4,22 @@ import java.awt.image.BufferedImage;
 
 public class SaveImageControl {
 
-	public static void saveBufferedImage(BufferedImage image){
+	/**
+	 * saves the image as cover for username	
+	 * @param image Buffered image
+	 * @param username for which user the cover is to be saved
+	 */
+	public static void saveBufferedImage(BufferedImage image, String username){
 		
 	}
 	
-	public static void saveBufferedImage(BufferedImage image, String username){
+	/**
+	 * save image in userdictory with specific filename(with extension)
+	 * @param image Buffered image
+	 * @param username for which user the image is to be saved
+	 * @param filename specific filename(with extension eg. "filename.jpg") 
+	 */
+	public static void saveBufferedImage(BufferedImage image, String username, String filename){
 		
 	}
 }
