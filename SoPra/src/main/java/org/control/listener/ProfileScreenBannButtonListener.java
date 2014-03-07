a5fda0595ff99b05401342ce87a8be94d7891f4b
Michael <michael.Pfennings@wwu.de>
2014-03-07 17:06:14 +0100

    Profile Admin buttons

---
+++ SoPra/src/main/java/org/control/listener/ProfileScreenBannButtonListener.java
@@ -0,0 +1,14 @@
+package org.control.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProfileScreenBannButtonListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
