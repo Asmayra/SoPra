59946fbf6cfdab249a374de650ea22fddde8dde3
t_lang20 <t_lang20@wwu.de>
2014-03-10 16:38:30 +0100

    testdatacreateion

--- SoPra/src/main/java/org/testpackage/createTestData.java
+++ SoPra/src/main/java/org/testpackage/createTestData.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import org.control.DatabaseControl;
+import org.control.PasswordControl;
 import org.model.Album;
 import org.model.Genre;
 import org.model.Label;
@@ -31,7 +32,7 @@ public class createTestData {
 		Post post = new Post();
 		
 		post.setAutor(usr);
-		post.setMessage("jsjhsjsjsk");
+		post.setMessage("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ");
 		
 		return post;
 	}
@@ -39,25 +40,10 @@ public class createTestData {
 		Label label = new Label();
 		label.setName("testlabel");
 		
-		User usrk = new User();
-		User usrl = new User();
 		
-		usrk.setUsername("k");
-		usrl.setUsername("l");
 		
-		try {
-			DatabaseControl.getInstance().save(usrk);
-			DatabaseControl.getInstance().save(usrl);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		usrl = null;
-		usrk = null;
-		
-		usrl = (User) DatabaseControl.getInstance().load(User.class, "l");
-		usrk = (User) DatabaseControl.getInstance().load(User.class, "k");
+		User usrl = (User) DatabaseControl.getInstance().load(User.class, "l");
+		User usrk = (User) DatabaseControl.getInstance().load(User.class, "k");
 		
 		label.addArtist(usrk);
 		label.addManager(usrl);
@@ -69,11 +55,10 @@ public class createTestData {
 			DatabaseControl.getInstance().save(label);
 			DatabaseControl.getInstance().update(usrk);
 			DatabaseControl.getInstance().update(usrl);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 
 
 	}
@@ -142,15 +127,15 @@ public class createTestData {
 		return song;
 	}
 	
-	public Message createMessage(User usr, User usr2){
+	public Message createMessage(User usr){
 		Message message = new Message();
-		message.setContent("balalalsjkdsklsjdslfdskl");		
+		message.setContent("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ");		
 		message.setDate("01.01.2000");
 		message.setSender(usr);
-		message.setSubject("testsubject");
-		//message.addRecipient(usr2);
+		message.setSubject("Lorem ipsum");
 		
 		try {
+			
 			DatabaseControl.getInstance().save(message);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -158,66 +143,41 @@ public class createTestData {
 		return message;
 	}
 	
-	public User createSingleUser(String username){
-		User standard = new User();
-		standard.setUsername(username);
-		standard.setEMail("muster@gmail.com");
-		standard.setCity("mustercity");
-		standard.setCountry("mustercountry");
+	public User createSingleUser(String username, String email, String city, String country, String dateString, String firstname, String lastname, String imagePath, String password, String rights){
+		User usr = new User();
+		usr.setUsername(username);
+		usr.setEMail(email);
+		usr.setCity(city);
+		usr.setCountry(country);
 		Date date;
 		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
+			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse(dateString);
 		} catch (ParseException e) {
 			date = null;
 			e.printStackTrace();
 		}
-		standard.setDob(date);
-		standard.setFirstname("Max");
-		standard.setLastname("mustermann");
-		standard.setImagePath("avatar.jpg");
-		standard.setPassword("127648922");
-		standard.setSalt("B150M)gWN!");
-		standard.setRights("StandardUser");
+		usr.setDob(date);
+		usr.setFirstname(firstname);
+		usr.setLastname(lastname);
+		usr.setImagePath(imagePath);
+		usr.setRights(rights);
+		
+		usr.setSalt(PasswordControl.generateSalt());
+		usr.setPassword(PasswordControl.encodePassword(password, usr.getSalt()));
+		
 		
 		try {
-			DatabaseControl.getInstance().save(standard);
+			DatabaseControl.getInstance().save(usr);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		return standard;
+		return usr;
 	}
 	
 	public void createUser(){
-		User standard = new User();
-		standard.setUsername("q");
-		standard.setEMail("muster@gmail.com");
-		standard.setCity("mustercity");
-		standard.setCountry("mustercountry");
-		Date date;
-		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
-		} catch (ParseException e) {
-			date = null;
-			e.printStackTrace();
-		}
-		standard.setDob(date);
-		standard.setFirstname("Max");
-		standard.setLastname("mustermann");
-		standard.setImagePath("avatar.jpg");
-		standard.setPassword("127648922");
-		standard.setSalt("B150M)gWN!");
-		standard.setRights("StandardUser");
-				
-		try {
-			DatabaseControl.getInstance().save(standard);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		standard.addMessage(createMessage(standard, createSingleUser("q2")));
-		DatabaseControl.getInstance().update(standard);
-		
+		User standard = createSingleUser("q", "MaxStandard@gmail.com", "Münster", "Deutschland", "10.01.1989", "Max", "Standard", "placeholder.jpg", "q", "StandardUser");
+			
 		standard.addPlaylist(createPlaylist(standard));
 		standard.addPlaylist(createPlaylist(standard));
 		standard.addAlben(createAlbum(standard));
@@ -229,59 +189,13 @@ public class createTestData {
 		/////////////////////////////////////////////////////
 	
 		
-		User admin = new User();
-		admin.setUsername("a");
-		admin.setEMail("muster@gmail.com");
-		admin.setCity("mustercity");
-		admin.setCountry("mustercountry");
-
-		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
-		} catch (ParseException e) {
-			date = null;
-			e.printStackTrace();
-		}
-		admin.setDob(date);
-		admin.setFirstname("Max");
-		admin.setLastname("mustermann");
-		admin.setImagePath("avatar.jpg");
-		admin.setPassword("127648922");
-		admin.setSalt("B150M)gWN!");
-		admin.setRights("Admin");
+		User admin = createSingleUser("a", "BillAdmin@gmail.com", "Berlin", "Deutschland", "20.04.1971", "Bill", "Admin", "placeholder.jpg", "a", "Admin");
 		
-		try {
-			DatabaseControl.getInstance().save(admin);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
 		
 		////////////////////////////////////////////////////////
 		
-		User artist = new User();
-		artist.setUsername("k");
-		artist.setEMail("muster@gmail.com");
-		artist.setCity("mustercity");
-		artist.setCountry("mustercountry");
-
-		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
-		} catch (ParseException e) {
-			date = null;
-			e.printStackTrace();
-		}
-		artist.setDob(date);
-		artist.setFirstname("Max");
-		artist.setLastname("mustermann");
-		artist.setImagePath("avatar.jpg");
-		artist.setPassword("127648922");
-		artist.setSalt("B150M)gWN!");
-		artist.setRights("Artist");
-
-		try {
-			DatabaseControl.getInstance().save(artist);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		User artist = createSingleUser("k", "JackArtist@gmail.com", "Moskau", "Russland", "14.10.1993", "Jack", "Artist", "placeholder.jpg", "k", "Artist");
+		
 		
 		artist.addOwnSong(this.createSong());
 		artist.addOwnSong(this.createSong());
@@ -289,86 +203,37 @@ public class createTestData {
 		artist.addFavorite(this.createSong());
 		artist.addPlaylists(this.createPlaylist(artist));
 		artist.addAlben(this.createAlbum(artist));
-		
-		artist.addMessage(createMessage(artist, standard));
 		DatabaseControl.getInstance().update(artist);
 		
-		artist.addMessage(createMessage(artist, admin));
+		artist.addMessage(createMessage(standard));
+		DatabaseControl.getInstance().update(artist);
+		
+		artist.addMessage(createMessage(standard));
 		DatabaseControl.getInstance().update(artist);
 		
 		///////////////////////////////////////////////////////////
 		
-		User labelmanager = new User();
-		labelmanager.setUsername("l");
-		labelmanager.setEMail("muster@gmail.com");
-		labelmanager.setCity("mustercity");
-		labelmanager.setCountry("mustercountry");
-
-		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
-		} catch (ParseException e) {
-			date = null;
-			e.printStackTrace();
-		}
-		labelmanager.setDob(date);
-		labelmanager.setFirstname("Max");
-		labelmanager.setLastname("mustermann");
-		labelmanager.setImagePath("avatar.jpg");
-		labelmanager.setPassword("127648922");
-		labelmanager.setSalt("B150M)gWN!");
-		labelmanager.setRights("LabelManager");
-		
-		try {
-			
-
-			DatabaseControl.getInstance().save(labelmanager);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		labelmanager.addMessage(createMessage(labelmanager, artist));
+		User labelmanager = createSingleUser("l", "JustinLabel@gmail.com", "New York", "USA", "23.06.1956", "Justin", "Label", "placeholder.jpg", "l", "LabelManager");
+		Message msg = createMessage(artist);
+		labelmanager.addMessage(msg);
+		//labelmanager.addMessage(createMessage(artist));
 		
 		DatabaseControl.getInstance().update(labelmanager);
 		
 		////////////////////////////////////////////////
 		
-		User artist1 = new User();
-		artist1.setUsername("k2");
-		artist1.setEMail("muster@gmail.com");
-		artist1.setCity("mustercity");
-		artist1.setCountry("mustercountry");
-
-		try {
-			date = new SimpleDateFormat("DD.MM.YYYY", Locale.GERMAN).parse("01.01.2000");
-		} catch (ParseException e) {
-			date = null;
-			e.printStackTrace();
-		}
-		artist1.setDob(date);
-		artist1.setFirstname("Max");
-		artist1.setLastname("mustermann");
-		artist1.setImagePath("avatar.jpg");
-		artist1.setPassword("127648922");
-		artist1.setSalt("B150M)gWN!");
-		artist1.setRights("Artist");
-		
-		try {
-			DatabaseControl.getInstance().save(artist1);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		User artist1 = createSingleUser("k2", "SamanthaArtist@gmail.com", "Tokio", "Japan", "27.12.2008", "Samantha", "Artist", "placeholder.jpg", "k2", "Artist");
 	
-		
+		/*
 		artist1.addOwnSong(this.createSong());
 		artist1.addOwnSong(this.createSong());
 		artist1.addFavorite(this.createSong());
 		artist1.addFavorite(this.createSong());
 		artist1.addPlaylists(this.createPlaylist(artist1));
 		artist1.addAlben(this.createAlbum(artist1));
-		artist1.addMessage(createMessage(artist1, artist));
+		artist1.addMessage(createMessage(artist));
 		DatabaseControl.getInstance().update(artist1);
-		
+		*/
 	}
 	
 }
