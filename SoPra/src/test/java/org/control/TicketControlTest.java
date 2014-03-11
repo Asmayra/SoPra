6b50ef70b0a82b47bda60bcc6277c6c2269aaa11
mats391 <m.schoenke@gmail.com>
2014-03-11 11:24:13 +0100

    TicketControlTest

---
+++ SoPra/src/test/java/org/control/TicketControlTest.java
@@ -0,0 +1,48 @@
+package org.control;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.model.Ticket;
+import org.model.User;
+
+public class TicketControlTest {
+
+	
+	@Test
+	public void createTicketTest()
+	{
+		String testCategory = "Testing";
+		String testTicketText = "Testing";
+		User testUser = new User();
+		testUser.setUsername("TicketTestUser");
+		
+		try {
+			DatabaseControl.getInstance().save(testUser);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertTrue(TicketControl.getInstance().createTicket(testCategory, testUser, testTicketText));
+		
+		assertFalse(TicketControl.getInstance().createTicket("", testUser, testTicketText));
+		
+		assertFalse(TicketControl.getInstance().createTicket(null, testUser, testTicketText));
+		
+		assertFalse(TicketControl.getInstance().createTicket(testCategory, null, testTicketText));
+		
+		assertTrue(TicketControl.getInstance().createTicket(testCategory, testUser, ""));
+		
+		for(Object t : DatabaseControl.getInstance().getTableContent("Ticket"))
+		{
+			if( ( (Ticket)t).getRequester().getUsername().equals("TicketTestUser") )
+				DatabaseControl.getInstance().delete(t);
+		}
+		
+		DatabaseControl.getInstance().delete(testUser);
+	}
+
+}
