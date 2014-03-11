6517a8b1d3167b106e206584cef2754acdf58100
t_mich04 <t_mich04@webstuhl.uni-muenster.de>
2014-03-11 14:35:47 +0100

    Suche

--- SoPra/src/main/java/org/control/SearchControl.java
+++ SoPra/src/main/java/org/control/SearchControl.java
@@ -2,11 +2,15 @@ package org.control;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.model.Label;
 import org.model.Song;
 import org.model.User;
+import org.view.DiscoverElement;
+import org.view.MainScreen;
+import org.view.screens.Center.SearchResultScreen;
 import org.view.screens.EastBar.EastBar;
 import org.view.screens.EastBar.SearchMask;
 
@@ -19,8 +23,43 @@ public class SearchControl {
 	 * @return List with results
 	 */
 	public void generateResults(String searchKeyword){	
-		List<?> resultList = databaseQueries(searchKeyword);
-				
+		ArrayList<User> userResults =(ArrayList<User>) DatabaseControl.getInstance().queryForKeyword(User.class, "username", searchKeyword);
+		ArrayList<Song> songResults =(ArrayList<Song>) DatabaseControl.getInstance().queryForKeyword(Song.class, "title", searchKeyword);
+		//ArrayList<Label> labelResults =(ArrayList<Label>) DatabaseControl.getInstance().queryForKeyword(Label.class, "name", searchKeyword);
+		int userNum=userResults.size();
+		int songNum=songResults.size();
+		//int labelNum=labelResults.size();
+		LinkedList<DiscoverElement> user = new LinkedList<DiscoverElement>();
+		LinkedList<DiscoverElement> labels = new LinkedList<DiscoverElement>();
+		LinkedList<DiscoverElement> songs = new LinkedList<DiscoverElement>();
+		for(int i=0;i<userNum;i++){
+			User current = userResults.get(i);
+			DiscoverElement curUser = new DiscoverElement(current.getUsername(),LoadImageControl.loadImageIcon(
+					current.getImagePath(), current),current.getUsername(),"User");
+			user.add(curUser);
+		}
+//		for(int i=0;i<labelNum;i++){
+//			Label current = labelResults.get(i);
+//			DiscoverElement curUser = new DiscoverElement(current.getName(),LoadImageControl.loadImageIcon(
+//					current.getImagePath(), current),current.getUsername(),"User");
+//			user.add(curUser);
+//		}
+		for(int i=0;i<songNum;i++){
+			Song recSong = songResults.get(i);
+			User usr = (User) DatabaseControl.getInstance().load(User.class, recSong.getInterpret());
+			DiscoverElement discover3mini = new DiscoverElement(recSong.getTitle(), LoadImageControl.loadImageIcon(
+					usr.getImagePath(), usr), usr.getUsername(), "Song");
+			songs.add(discover3mini);
+		}
+		
+		SearchResultScreen screen = SearchResultScreen.getInstance();
+		screen.setDiscoverArtists(user);
+		screen.setDiscoverSongs(songs);
+		screen.setDiscoverLabels(labels);
+		screen.showRecommendations();
+		
+		MainScreen.getInstance().updateCenter(screen);
+		
 	}
 	
 	/**
---
+++ SoPra/src/main/java/org/view/screens/Center/SearchResultScreen.java
@@ -0,0 +1,115 @@
+package org.view.screens.Center;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.LinkedList;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import org.view.DiscoverElement;
+
+/**
+ * @author Tim Michels
+ */
+public class SearchResultScreen extends JPanel{
+	
+	private LinkedList<DiscoverElement> searchArtists;
+	private LinkedList<DiscoverElement> searchLabels;
+	private LinkedList<DiscoverElement> searchSongs;
+	private JScrollPane scrolResults;
+	private JPanel resultPane = new JPanel();
+	
+	private static SearchResultScreen instance = null;
+	
+	/**
+	 * 
+	 * @return Instanz des SearchResultScreen
+	 */
+	public static SearchResultScreen getInstance()
+	{
+		if(instance == null){
+			instance = new SearchResultScreen();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Zerstört den Singleton
+	 */
+	public static void destroy()
+	{
+		instance = null;
+	}
+	
+	/**
+	 * Konstruktor
+	 */
+	public SearchResultScreen(){
+		resultPane.setLayout(new GridBagLayout());
+		scrolResults= new JScrollPane(resultPane);
+		this.add(scrolResults);
+		this.setPreferredSize(new Dimension(500,500));
+	}
+	
+	/**
+	 * Anordnen der Empfehlungen
+	 */
+	public void showRecommendations(){
+		resultPane.removeAll();
+		int aSize = searchSongs.size();
+		int bSize= searchLabels.size();
+		int cSize=searchArtists.size();
+		try{
+		GridBagConstraints c = new GridBagConstraints();
+		c.ipadx=20;
+		c.ipady=20;
+		
+		for(int i=0;i<aSize;i++){
+			
+			c.gridx=(i%3);
+			c.gridy=(i/3);
+			resultPane.add(searchSongs.get(i),c);
+		}
+		for(int i=aSize;i<aSize+bSize;i++){
+			
+			c.gridx=(i%3);
+			c.gridy=(i/3);
+			resultPane.add(searchLabels.get(i-aSize),c);
+		}
+		for(int i=aSize+bSize;i<aSize+bSize+cSize;i++){
+			
+			c.gridx=(i%3);
+			c.gridy=(i/3);
+			resultPane.add(searchArtists.get(i-aSize-bSize),c);
+		}
+		
+		}catch(NullPointerException exc){System.err.println("error");}
+	}
+	
+	public LinkedList<DiscoverElement> getDiscoverArtists() {
+		return searchArtists;
+	}
+
+	public void setDiscoverArtists(LinkedList<DiscoverElement> searchArtists) {
+		this.searchArtists = searchArtists;
+	}
+
+	public LinkedList<DiscoverElement> getDiscoverLabels() {
+		return searchLabels;
+	}
+
+	public void setDiscoverLabels(LinkedList<DiscoverElement> searchAlbums) {
+		this.searchLabels = searchAlbums;
+	}
+
+	public LinkedList<DiscoverElement> getDiscoverSongs() {
+		return searchSongs;
+	}
+
+	public void setDiscoverSongs(LinkedList<DiscoverElement> searchSongs) {
+		this.searchSongs = searchSongs;
+	}
+	
+}
